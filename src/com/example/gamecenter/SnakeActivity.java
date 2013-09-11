1e3cd92c46a2cad83b667a7f7f579ffe6a248477
shaoqiu <xushaoqiu220@gail.com>
2013-09-11 16:54:59 +0800

    add keydown event

--- src/com/example/gamecenter/SnakeActivity.java
+++ src/com/example/gamecenter/SnakeActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.GestureDetector;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,21 +14,41 @@ import android.widget.TextView;
 
 public class SnakeActivity extends Activity {
 
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		switch (keyCode) {
+		case KeyEvent.KEYCODE_DPAD_UP:
+			snake.addDirToQue(Integer.valueOf(snake.UP));
+			break;
+		case KeyEvent.KEYCODE_DPAD_RIGHT:
+			snake.addDirToQue(Integer.valueOf(snake.RIGHT));
+			break;
+		case KeyEvent.KEYCODE_DPAD_DOWN:
+			snake.addDirToQue(Integer.valueOf(snake.DOWN));
+			break;
+		case KeyEvent.KEYCODE_DPAD_LEFT:
+			snake.addDirToQue(Integer.valueOf(snake.LEFT));
+			break;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+
 	private GestureDetector gestureDetector;
 	private SnakeView snake;
 	private ImageButton statusButton;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		
+
 		requestWindowFeature(Window.FEATURE_NO_TITLE); 
-		
+
 		this.setContentView(R.layout.snake);
 		statusButton = (ImageButton)findViewById(R.id.status_button);
 		statusButton.setBackgroundColor(Color.argb(0, 0, 0, 0));
-//		statusButton.setImageResource(R.drawable.start);
+		//		statusButton.setImageResource(R.drawable.start);
 		statusButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -38,64 +59,64 @@ public class SnakeActivity extends Activity {
 				}
 				else if(snake.getstatus() == snake.RUNNING)
 				{
-//					statusButton.setText("start");
+					//					statusButton.setText("start");
 					statusButton.setImageResource(R.drawable.start);
 					snake.pause();
 				}
 				else
 				{
-//					statusButton.setText("pause");
+					//					statusButton.setText("pause");
 					statusButton.setImageResource(R.drawable.pause);
 					snake.start();
 				}
 			}
 		});
-		
+
 		snake = (SnakeView)findViewById(R.id.snake);
 		snake.scoreTextView = (TextView)findViewById(R.id.scoreTextView);
 		snake.restTimeTextView = (TextView)findViewById(R.id.restTimeTextView);
-		
+
 		gestureDetector = new BuileGestureExt(this,new BuileGestureExt.OnGestureResult() {
-            @Override
-            public void onGestureResult(int direction) {
-            	
-            	if (snake.getstatus() == snake.READY)
-            	{
-            		snake.initNewGame();
-            		statusButton.setImageResource(R.drawable.pause);
-            		return;
-            	}
-            	if(snake.getstatus() != snake.RUNNING)
-            	{
-            		return;
-            	}
-            	
-            	
-            	if (direction == BuileGestureExt.GESTURE_UP)
-            	{
-            		snake.addDirToQue(new Integer(snake.UP));
-            	}
-            	else if (direction == BuileGestureExt.GESTURE_DOWN)
-            	{
-            		snake.addDirToQue(new Integer(snake.DOWN));
-            	}
-            	else if (direction == BuileGestureExt.GESTURE_LEFT)
-            	{
-            		snake.addDirToQue(new Integer(snake.LEFT));
-            	}
-            	else if (direction == BuileGestureExt.GESTURE_RIGHT)
-            	{
-            		snake.addDirToQue(new Integer(snake.RIGHT));
-            	}
-            }
-        }
-        ).Buile();
+			@Override
+			public void onGestureResult(int direction) {
+
+				if (snake.getstatus() == snake.READY)
+				{
+					snake.initNewGame();
+					statusButton.setImageResource(R.drawable.pause);
+					return;
+				}
+				if(snake.getstatus() != snake.RUNNING)
+				{
+					return;
+				}
+
+
+				if (direction == BuileGestureExt.GESTURE_UP)
+				{
+					snake.addDirToQue(Integer.valueOf(snake.UP));
+				}
+				else if (direction == BuileGestureExt.GESTURE_DOWN)
+				{
+					snake.addDirToQue(Integer.valueOf(snake.DOWN));
+				}
+				else if (direction == BuileGestureExt.GESTURE_LEFT)
+				{
+					snake.addDirToQue(Integer.valueOf(snake.LEFT));
+				}
+				else if (direction == BuileGestureExt.GESTURE_RIGHT)
+				{
+					snake.addDirToQue(Integer.valueOf(snake.RIGHT));
+				}
+			}
+		}
+				).Buile();
 
 	}
 
 	@Override
-    public boolean onTouchEvent(MotionEvent event) {
-        return gestureDetector.onTouchEvent(event);
-    }
+	public boolean onTouchEvent(MotionEvent event) {
+		return gestureDetector.onTouchEvent(event);
+	}
 
 }
